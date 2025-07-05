2acf57d86b1a7b0c5fb4314ed991142511769735
ritik kumar <148605032+ritik-kumar123@users.noreply.github.com>
2025-07-05 09:33:32 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,87 @@
+let boxes = document.querySelectorAll(".box");
+let resetbtn= document.querySelector("#reset-btn");
+let newGameBtn= document.querySelector("#new-btn");
+let div= document.querySelector("#winner");
+ let turnO = true;  // playerX, playerO
+ const winPatterns =[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+ ];
+ boxes.forEach((box) => {
+    box.addEventListener("click",() =>
+    {
+      if(turnO)   
+      {
+        box.innerText ="O";
+        turnO=false;
+      }
+      else
+      {
+        box.innerText ="X";
+        turnO=true;
+      }  
+      box.disabled = true; 
+      checkWinner();
+    })
+ });
+ const disabledboxes =() =>
+ {
+  for(let box of boxes)
+  {
+    box.disabled=true;
+  }
+ }
+ const enabledboxes =() =>
+ {
+  for(let box of boxes)
+  {
+    box.disabled=false;
+    box.innerText=" ";
+  }
+ }
+resetbtn.addEventListener("click",()=>
+{
+  enabledboxes();
+})
+newGameBtn.addEventListener("click",()=>
+{
+enabledboxes();
+div.innerHTML=" ";
+resetbtn.classList.remove("hide");
+newGameBtn.classList.add("hide");
+})
+
+const winner =(name)=>
+ {
+  div.append("Congratulation "+name+" is winner");
+  let btn = document.querySelector(".hide");
+  newGameBtn.classList.remove("hide");
+  resetbtn.classList.add("hide")
+ }
+ const reset =()=>
+ {
+   enabledboxes();
+ }
+ const checkWinner = () =>
+    {
+        for (pattern of winPatterns)
+            {
+                let pos1=boxes[pattern[0]].innerText;
+                let pos2=boxes[pattern[1]].innerText;
+                let pos3=boxes[pattern[2]].innerText;
+                if(pos1 !="" && pos2 !="" && pos3 !="")
+                {
+                   if(pos1=== pos2 && pos2 === pos3)
+                    {
+                      winner(pos1);
+                     disabledboxes(); 
+                    } 
+                }
+            }   
+    }    
